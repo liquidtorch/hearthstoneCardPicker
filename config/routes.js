33139e3db3183082da0e7f14759377e80bd2afa7
Andy Zalit <andyzalit@gmail.com>
2018-10-01 10:52:03 -0700

    works until more than 3 cards in My Cards & remove does not work properly

--- config/routes.js
+++ config/routes.js
@@ -6,4 +6,7 @@ module.exports = function(app){
 
   app.post('/cards', cards.new_card);
 
+  app.get('/cards/add/:id', cards.add);
+
+  app.get('/cards/remove/:id', cards.remove);
 }
--- controllers/cards.js
+++ controllers/cards.js
@@ -3,11 +3,14 @@ const knex = require("../db/knex.js");
 module.exports = {
 
   index: (req, res)=> {
-    knex('cards').orderBy('id').then((results) => {
-      res.render('index', {cards: results});
+    if(!req.session.deck){
+      req.session.deck = [];
+    }
+    knex('cards').orderBy('id')
+    .then((results) => {
+      res.render('index', {cards: results, deck: req.session.deck || []});
     })
   },
-
 
   new_card: (req, res)=> {
     knex('cards').insert({
@@ -18,5 +21,33 @@ module.exports = {
     }).then(()=> {
       res.redirect('/');
     })
+  },
+
+  add: (req, res) => {
+    knex('cards')
+    .where('id', req.params.id)
+    .then((results) => {
+      req.session.deck.push(results[0]);
+      res.redirect('/');
+    })
+  },
+
+  remove: (req, res) => {
+    let deck = req.session.deck
+    console.log(deck.length);
+    if(deck.length == 1){
+      req.session.deck = [];
+      res.redirect('/');
+      return;
+    }
+    for(let i = 0; i < deck.length; i++){
+      if(deck[i].id == req.params.id){
+        console.log(deck[i]);
+        deck.splice(i, 1);
+        res.redirect('/');
+        return;
+      }
+    }
+    res.redirect('/');
   }
 }
